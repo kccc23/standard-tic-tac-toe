6e4e4fda1b101d6afd7435fde140cb282b26252d
Kaining <kainingchen2013@gmail.com>
2023-02-09 12:32:56 -0500

    Refactored a game_over function

--- game.py
+++ game.py
@@ -15,6 +15,14 @@ def print_board(entries):
     print(output)
     print()
 
+def game_over(board, i):
+
+    print_board(board)
+    print("GAME OVER")
+    print(board[i], "has won")
+    exit()
+
+    return
 
 board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 current_player = "X"
@@ -26,37 +34,21 @@ for move_number in range(1, 10):
     board[space_number] = current_player
 
     if board[0] == board[1] and board[1] == board[2]:
-        print_board(board)
-        print(board[0], "has won")
-        exit()
+        game_over(board, 0)
     elif board[3] == board[4] and board[4] == board[5]:
-        print_board(board)
-        print(board[3], "has won")
-        exit()
+        game_over(board, 3)
     elif board[6] == board[7] and board[7] == board[8]:
-        print_board(board)
-        print(board[6], "has won")
-        exit()
+        game_over(board, 6)
     elif board[0] == board[3] and board[3] == board[6]:
-        print_board(board)
-        print(board[0], "has won")
-        exit()
+        game_over(board, 0)
     elif board[1] == board[4] and board[4] == board[7]:
-        print_board(board)
-        print(board[1], "has won")
-        exit()
+        game_over(board, 1)
     elif board[2] == board[5] and board[5] == board[8]:
-        print_board(board)
-        print(board[2], "has won")
-        exit()
+        game_over(board, 2)
     elif board[0] == board[4] and board[4] == board[8]:
-        print_board(board)
-        print(board[0], "has won")
-        exit()
+        game_over(board, 0)
     elif board[2] == board[4] and board[4] == board[6]:
-        print_board(board)
-        print(board[0], "has won")
-        exit()
+        game_over(board, 2)
 
     if current_player == "X":
         current_player = "O"
